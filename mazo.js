50d6248c5808dca1d8282e30e3a5914a03b224cd
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-08 15:36:23 -0300

    renamed vars

--- mazo.js
+++ mazo.js
@@ -1,73 +1,75 @@
-const armar = document.getElementById('armar') 
-const jugar = document.getElementById('jugar')
+const jugar = document.getElementById("play");
 
+// Generate random num between min and max
 const randomNum = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-const mazoCompleto = [];
-const palos = ['Espada', 'Basto', 'Oro', 'Copa']
+const completeDeck = [];
+const symbols = ["Espada", "Basto", "Oro", "Copa"];
+const validNums = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
 
-//ARRAY CON NUMEROS VALIDOS
 //Generar mazo completo
-for (let i = 0; i < palos.length; i++) {
-    for (let j = 0; j < 12; j++) {
-        mazoCompleto.push({ numero: j + 1, palo: `${palos[i]}` })
-    }
+
+for (let i = 0; i < symbol.length; i++) {
+  for (let j = 0; j < 12; j++) {
+    completeDeck.push({ number: j + 1, symbol: `${symbol[i]}` });
+  }
 }
 
-let mazoTruco = mazoCompleto.filter(carta => carta.numero != 8 && carta.numero != 9)
+let trucoDeck = completeDeck.filter(
+  (card) => card.number != 8 && card.number != 9
+);
 
 //Partida de altisima
-let jugadores = 4
-let cartasPorRonda = [];
+let players = 4;
+let cardsPerRound = [];
 let roundCounter = 0;
-const rondasDefault = 9;
+const defaultRounds = 9;
 const maxCardsInDeck = 40;
 
-const armarRondas = (jug) => {
-    for (let i = 0; i < rondasDefault; i++) {
-        let maxCardsByRound = (maxCardsInDeck / jug) 
-        const minCardsByRound = 3;
-        cartasPorRonda[i] = randomNum(minCardsByRound, maxCardsByRound + 1)
-    }
-    console.log(cartasPorRonda);
-}
+const makeRounds = (players) => {
+  for (let i = 0; i < defaultRounds; i++) {
+    let maxCardsByRound = maxCardsInDeck / players;
+    const minCardsByRound = 3;
+    cardsPerRound[i] = randomNum(minCardsByRound, maxCardsByRound + 1);
+  }
+  console.log(cardsPerRound);
+};
 
 //Repartir cartas a cada jugador
-const repartir = (cardsInRound, mazoTrucoCopia) => {
-
-    for (let i = 0; i < cardsInRound; i++) {
-        let indice = randomNum(0, 40)
-        let carta = mazoTrucoCopia[indice];
-        if (carta !== undefined) {
-            mazoTrucoCopia.splice(indice, 1);
-            console.log(`${i},`, `${carta.numero} de ${carta.palo},`, `ml: ${mazoTrucoCopia.length}.`);
-        } else {
-            i = i - 1;
-        }
+const deal = (cardsInRound, mazoTrucoCopia) => {
+  for (let i = 0; i < cardsInRound; i++) {
+    let indice = randomNum(0, 40);
+    let carta = mazoTrucoCopia[indice];
+    if (carta !== undefined) {
+      mazoTrucoCopia.splice(indice, 1);
+      console.log(
+        `${i},`,
+        `${carta.numero} de ${carta.palo},`,
+        `ml: ${mazoTrucoCopia.length}.`
+      );
+    } else {
+      i = i - 1;
     }
-}
+  }
+};
 
 //Cada ronda
-const rondas = (round) => {
-    let mazoTrucoCopia = [].concat(mazoTruco)
-    for (let i = 0; i < jugadores; i++) {
-        repartir(cartasPorRonda[round], mazoTrucoCopia);
-        console.log('****************');
-    }
-}
+const rounds = (round) => {
+  let mazoTrucoCopia = [].concat(trucoDeck);
+  for (let i = 0; i < players; i++) {
+    deal(cardsPerRound[round], mazoTrucoCopia);
+    console.log("****************");
+  }
+};
 
-const juego = () => {
-    console.log(`RONDA ${roundCounter + 1}`)
-    rondas(roundCounter);
-    roundCounter += 1;
-}
+const game = () => {
+  console.log(`RONDA ${roundCounter + 1}`);
+  rounds(roundCounter);
+  roundCounter += 1;
+};
 
-armar.addEventListener('click', armarRondas(jugadores));
-jugar.addEventListener('click', juego);
-
-//***************************** */
-//******REINICIAR MAZO EN CADA RONDA */
+jugar.addEventListener("click", game);
